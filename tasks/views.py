36ceae134196a1cc03db02b8fdd842326b54310b
Oscar Sabet <osabet021@gmail.com>
2025-03-26 14:19:52 +0000

    add board view

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Task
 
 # Create your views here.
 def tasks(request):
     return HttpResponse("Hello, DoodleTo tasks!")
+
+
+def board(request):
+    
+    order_by = request.GET.get("order_by", "created")
+    tasks = Task.objects.order_by(order_by)
+    pending = tasks.filter(status="P")
+    in_progress = tasks.filter(status="IP")
+    completed = tasks.filter(status="C")
+    
+    return render(request, "** insert template **", {
+        "pending": pending,
+        "in_progress": in_progress,
+        "completed": completed,
+    })
