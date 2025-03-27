6cb33dc65881361ed416f0189c348f71ef36dd66
Oscar Sabet <osabet021@gmail.com>
2025-03-27 09:49:47 +0000

    add task view

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Task
+from django.contrib.auth.decorators import login_required
+
 
 # Display the list of tasks
 def task_list(request):
@@ -28,3 +30,36 @@ def delete_task(request, task_id):
     task = Task.objects.get(id=task_id)
     task.delete()
     return redirect('task_list')
+
+
+@login_required
+def board(request):
+    """
+    Renders a task board view, categorizing tasks by their status.
+
+    This view retrieves all tasks from the database, orders them based on
+    a query parameter, and filters them into three categories: Pending,
+    In Progress, and Completed. The categorized tasks are then passed to the
+    template for rendering.
+
+    Args:
+        request (HttpRequest): The HTTP request object containing metadata
+        about the request, including GET parameters.
+
+    Returns:
+        HttpResponse: A rendered HTML page displaying tasks grouped by their
+        status (Pending, In Progress, Completed).
+    """
+    tasks = Task.objects.filter(user=request.user)
+    tasks = Task.objects.order_by("due_date")
+
+    pending = tasks.filter(status="P")
+    in_progress = tasks.filter(status="IP")
+    completed = tasks.filter(status="C")
+
+    return render(request, "** insert template **", {
+        "tasks": tasks,  # May not be needed
+        "pending": pending,
+        "in_progress": in_progress,
+        "completed": completed,
+    })
