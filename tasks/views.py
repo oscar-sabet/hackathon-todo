7f0dfb71f1a00c750873a8dcaec645ef42dfdc96
Oliver-William <ad.astra321@hotmail.com>
2025-03-27 09:27:43 +0000

    Add views for CRUD functionality

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Task
 
+# Display the list of tasks
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'task_list.html', {'tasks': tasks})
 
-# Create your views here.
-def tasks(request):
-    return HttpResponse("Hello, DoodleTo tasks!")
+# Add a new task
+def add_task(request):
+    if request.method == 'POST':
+        task_title = request.POST.get('title')
+        if task_title:
+            Task.objects.create(title=task_title)
+        return redirect('task_list')
+    return render(request, 'add_task.html')
+
+# Mark a task as completed
+def mark_completed(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.completed = True
+    task.save()
+    return redirect('task_list')
+
+# Delete a task
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('task_list')
