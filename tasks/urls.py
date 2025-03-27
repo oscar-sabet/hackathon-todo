5df88ede0ea8b25f6910d32ae808c81ade20779a
Oscar Sabet <osabet021@gmail.com>
2025-03-27 10:56:37 +0000

    update branch

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     # path('', views.index, name='index'),
     path('tasks', views.task_list, name='task_list'),
+    path("delete_task/<int:task_id>/", views.delete_task, name="delete_task"),
+
 ]
