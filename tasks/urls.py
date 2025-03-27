4e449ac24b833a5cafb27ec6cf2ae6e8f3f55970
Oscar Sabet <osabet021@gmail.com>
2025-03-27 09:54:51 +0000

    fix urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.tasks, name='tasks'),
+    # path('', views.index, name='index'),
+    path('tasks', views.task_list, name='task_list'),
 ]
