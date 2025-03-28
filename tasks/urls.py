466f00edc5907f31125c3513a1d497119a33af82
francescawaters <francescawaters26@outlook.com>
2025-03-28 17:08:32 +0000

    fix complete checkbox functionality

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,10 +7,6 @@ urlpatterns = [
     path("add_task/", views.add_task, name="add_task"),
     path("board/", views.board, name="board"),
     path("delete_task/<int:task_id>/", views.delete_task, name="delete_task"),
-    path(
-        "mark_completed/<int:task_id>/",
-        views.mark_completed,
-        name="mark_completed",
-    ),
     path("edit_task/<int:task_id>/", views.edit_task, name="edit_task"),
+    path('tasks/mark_completed/<int:task_id>/', views.mark_completed, name='mark_completed'),
 ]
